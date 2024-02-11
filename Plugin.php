8f1f9c94cbf6b9f5f47cfd7f22f0d1d4a7d0429b
Manuel <info@tagirijus.de>
2024-02-11 22:44:16 +0100

    Prepared v1.2.0

--- Plugin.php
+++ Plugin.php
@@ -58,7 +58,7 @@ class Plugin extends Base
 
     public function getPluginVersion()
     {
-        return '1.1.1';
+        return '1.2.0';
     }
 
     public function getCompatibleVersion()
--- changelog.md
+++ changelog.md
@@ -1,6 +1,14 @@
 # Changelog
 
 
+## v1.2
+
+### What's Changed
+
+_(most recent changes are listed on top):_
+- Added copy-button next to modified url
+
+
 ## v1.1
 
 ### What's Changed
